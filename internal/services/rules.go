54f3adf19675506ce85a2e3e6a3d160edf5a136e
Isuru-F <61958023+Isuru-F@users.noreply.github.com>
2025-08-27 15:58:50 +1000

    revert(rules): restore constants to baseline values for main vs migration comparison

--- internal/services/rules.go
+++ internal/services/rules.go
@@ -1,11 +1,11 @@
 package services
 
 const (
-	MaxDistinctCartItems    = 4
+	MaxDistinctCartItems    = 3
 	MaxQuantityPerLineItem  = 5
 	MaxTotalItemsInCart     = 10
-	CartRiskLimitTotal      = 6000.0
-	MinOrderAmount          = 6.0
+	CartRiskLimitTotal      = 5000.0
+	MinOrderAmount          = 5.0
 	HighValueReviewThreshold = 3000.0
 	DailyUserSpendCap       = 10000.0
 	DuplicateOrderWindowSec = 5
